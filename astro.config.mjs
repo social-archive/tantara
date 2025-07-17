882410f864d275ba2b13214f8f9377e33a65a724
marvin-seo-dev <dj500dj100@gmail.com>
2025-07-17 11:08:29 +0900

    dd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     outDir: './docs', // ← 빌드 결과를 docs 폴더로 지정
-    base: '/',        // 루트 경로에서 배포 (또는 '/레포명/'으로 수정)
+    base: '/tantara/',        // 루트 경로에서 배포 (또는 '/레포명/'으로 수정)
     vite: {
       plugins: [tailwindcss()],
     },
