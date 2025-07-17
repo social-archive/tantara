664de1bd9faaebe79e2de4b107aff6dfcd4e549b
marvin-seo-dev <dj500dj100@gmail.com>
2025-07-17 12:37:23 +0900

    딴따라

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/typography')],
+  };
+  
