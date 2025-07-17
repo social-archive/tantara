4d90cbe3f653c41c5ec3d471940d59079f016232
marvin-seo-dev <dj500dj100@gmail.com>
2025-07-17 13:18:13 +0900

    dd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     outDir: './docs', // ← 빌드 결과를 docs 폴더로 지정
-    base: '/tantara/',        // 루트 경로에서 배포 (또는 '/레포명/'으로 수정)
+    // base: '/tantara/',        // 루트 경로에서 배포 (또는 '/레포명/'으로 수정)
     vite: {
       plugins: [tailwindcss()],
     },
